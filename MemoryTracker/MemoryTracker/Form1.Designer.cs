abe0a12cc86f44f85fc36b15a5453a1e4fc2fad0
Najir Pandey <nepalinazeer@hotmail.com>
2015-07-30 16:50:10 -0400

    Version 1.0
    A simple multi-threaded program that keeps track of current memory usage
    and the peak usage

---
+++ MemoryTracker/MemoryTracker/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace MemoryTracker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxPeak = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxInitial = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(29, 150);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(57, 52);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBoxPeak
+            // 
+            this.textBoxPeak.Location = new System.Drawing.Point(57, 105);
+            this.textBoxPeak.Name = "textBoxPeak";
+            this.textBoxPeak.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPeak.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Current:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Peak:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(119, 155);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Inactive";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(40, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(130, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "--Memory Usage Tracker--";
+            // 
+            // textBoxInitial
+            // 
+            this.textBoxInitial.Location = new System.Drawing.Point(57, 79);
+            this.textBoxInitial.Name = "textBoxInitial";
+            this.textBoxInitial.Size = new System.Drawing.Size(100, 20);
+            this.textBoxInitial.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(19, 82);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Initial:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(163, 55);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(22, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "GB";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(163, 82);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(22, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "GB";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(163, 108);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(22, 13);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "GB";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(211, 203);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxInitial);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxPeak);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Memory Tracker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBoxPeak;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxInitial;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ MemoryTracker/MemoryTracker/Form1.cs
@@ -0,0 +1,143 @@
+﻿//A simple multi-threaded program that keeps track of current memory usage and the peak usage
+//Author: Najir Pandey
+//website: www.nepalinazeer.com
+
+
+using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace MemoryTracker
+{
+    public partial class Form1 : Form
+    {
+
+        private BackgroundWorker m_AsyncWorker = new BackgroundWorker();
+
+        private string _memUsage = "0.0";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _memUsage = performanceMonitor().ToString(CultureInfo.InvariantCulture);
+
+            textBox1.Text = _memUsage;
+            textBoxPeak.Text = _memUsage;
+            textBoxInitial.Text = _memUsage;
+
+            m_AsyncWorker.WorkerReportsProgress = true;
+            m_AsyncWorker.WorkerSupportsCancellation = true;
+            m_AsyncWorker.ProgressChanged += backgroundWorker1_ProgressChanged;
+            m_AsyncWorker.RunWorkerCompleted +=backgroundWorker1_RunWorkerCompleted;
+            m_AsyncWorker.DoWork +=backgroundWorker1_DoWork;
+
+            //execute the tracker during initialization
+            ExecuteTracker();
+
+        }
+   
+        private void ExecuteTracker()
+        {
+            if (m_AsyncWorker.IsBusy)
+            {
+                button1.Enabled = false;
+                label3.Text = "Stopping...";
+
+                m_AsyncWorker.CancelAsync();
+            }
+            else
+            {
+                button1.Text = "Stop";
+                label3.Text = "Running...";
+
+                m_AsyncWorker.RunWorkerAsync();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            ExecuteTracker();
+            
+                        
+        }
+
+        private double performanceMonitor()
+        {
+
+        
+                Int64 phav = PerformanceInfo.GetPhysicalAvailableMemoryInMiB();
+                Int64 tot = PerformanceInfo.GetTotalMemoryInMiB();
+                decimal percentFree = ((decimal)phav / (decimal)tot) * 100;
+                decimal percentOccupied = 100 - percentFree;
+
+
+                return ((double)(tot - phav) / 1024);
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+            BackgroundWorker backgroundWorker1 = sender as BackgroundWorker;
+                      
+            double currentUsage = performanceMonitor();
+
+     
+            while(true)
+            {
+                Thread.Sleep(100);
+                currentUsage = performanceMonitor();
+                e.Result = currentUsage.ToString();
+                _memUsage = currentUsage.ToString();
+
+               backgroundWorker1.ReportProgress(1);
+
+                if (backgroundWorker1.CancellationPending)
+                {
+                    Thread.Sleep(1000);
+
+                    e.Cancel = true;
+
+                    return;
+
+                }
+            }
+
+        
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            button1.Text = "Start";
+            button1.Enabled = true;
+
+          
+            if (e.Error != null)
+            {
+                MessageBox.Show(e.Error.Message);
+                return;
+            }
+
+            label3.Text = e.Cancelled ? "Stopped..." : "Completed...";
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            textBox1.Text = _memUsage;
+        
+            if (Convert.ToDouble(textBoxPeak.Text) < Convert.ToDouble(_memUsage))
+            {
+                textBoxPeak.Text = _memUsage;
+            }
+        }
+
+    }
+
+}
+
+
+
+
